44a40d1cdaf6178bc2f8b733a185321cbb3bbcec
Administrator@Manu <emmanuelochieng35@gmail.com>
2025-08-09 00:21:30 +0300

    Initial commit - fresh start

---
+++ dict.py
@@ -0,0 +1,11 @@
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print (capital_city)
+# Output: {'Nepal': 'Kathmandu', 'Italy': 'Rome', 'England': 'London'}
+
+student_id = {112, 114, 116, 118, 115}
+print( 'Student ID: ', student_id)
+
+
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters: ', vowel_letters)
+# Output: Student ID:  {112, 114, 115, 116, 118} 
---
+++ student_set.py
@@ -0,0 +1,35 @@
+student_id = {112, 114, 116, 118, 115}
+print( 'Student ID: ', student_id)
+
+
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters: ', vowel_letters)
+
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types: ', mixed_set)
+
+empty_set = set()
+print('Empty Set: ', empty_set)
+
+empty_dictionary = {}
+print('Empty Dictionary: ', empty_dictionary)
+
+numbers = {2, 4, 6, 6, 2, 8}
+print(numbers) 
+
+numbers = {21, 34, 54, 12}
+print('Initial Set:' ,numbers)
+numbers.add(32)
+print('Set after adding an element:', numbers)
+
+A = {1, 3, 5}
+
+B = {0, 2, 4}
+
+print( 'union using |:', A | B)
+
+name = "Python"
+print(name)
+message = "I love python"
+print(message)
+
